a8a860e689203e2738a3721c4c5d561d0104be20
pwildenhain <35195136+pwildenhain@users.noreply.github.com>
2019-05-20 22:53:32 -0400

    :bug: Refer to conf.py for build instructions

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,6 +41,9 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+# Master doc for build
+master_doc = 'index'
+
 
 # -- Options for HTML output -------------------------------------------------
 
