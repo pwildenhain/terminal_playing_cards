b182a00f5acee12235077a08df06cdf8e1b9cb2a
Paul <wildenhaip@email.chop.edu>
2019-06-17 21:25:50 -0400

    :recycle: Refactor merge method to use class attributes

--- TerminalPlayingCards/__main__.py
+++ TerminalPlayingCards/__main__.py
@@ -7,6 +7,5 @@ MY_CARDS = [card.Card("A", "clubs", 0), card.Card("Q", "hearts", 0)]
 MY_HAND = view.View(MY_CARDS)
 
 if __name__ == "__main__":
-    pprint(MY_HAND._merge_right())
     pprint(str(MY_HAND))
     print(MY_HAND)
--- TerminalPlayingCards/view.py
+++ TerminalPlayingCards/view.py
@@ -7,17 +7,49 @@ from TerminalPlayingCards.utils import convert_layers_to_string
 class View:
     """View one or more playing cards in the terminal"""
 
-    def __init__(self, cards: list):
+    def __init__(
+        self, cards: list, merge_direction: str = "right", merge_padding: int = 2
+    ):
         self.cards = cards
+        self._merge_direction = None
+        self.merge_direction = merge_direction
+        self._merge_padding = None
+        self.merge_padding = merge_padding
+
+    @property
+    def merge_direction(self):
+        """Get the merge direction of the View"""
+        return self._merge_direction
+
+    @merge_direction.setter
+    def merge_direction(self, direction: str):
+        if direction in ["right", "left", "up", "down"]:
+            self._merge_direction = direction
+        else:
+            raise NotImplementedError(f"The View class cannot merge cards {direction}")
+
+    @property
+    def merge_padding(self):
+        "Get the amount of padding between cards in the View"
+        return self._merge_padding
+
+    @merge_padding.setter
+    def merge_padding(self, padding: int):
+        if padding > 0:
+            self._merge_padding = padding
+        else:
+            raise NotImplementedError(
+                "The View class cannot have merge padding less than zero"
+            )
 
     def __len__(self) -> int:
         """Return the number of cards in the View"""
         return len(self.cards)
 
-    def _merge_right(self, padding: int = 2) -> list:
+    def _merge_right(self) -> list:
         """Merge all cards in the View to the right of each other"""
         merged_grid = []
-        padding = [Style.RESET_ALL] + [" " for _ in range(padding)]
+        padding = [Style.RESET_ALL] + [" " for _ in range(self._merge_padding)]
 
         for layer in range(7):
             merged_layer = []
@@ -29,25 +61,23 @@ class View:
 
         return merged_grid
 
-    def _merge_left(self, padding: int = 2) -> list:
+    def _merge_left(self) -> list:
         """Merge all cards in the View to the left of each other"""
         pass
 
-    def _merge_up(self, padding: int = 2) -> list:
+    def _merge_up(self) -> list:
         """Merge all cards in the View over each other"""
         pass
 
-    def _merge_down(self, padding: int = 2) -> list:
+    def _merge_down(self) -> list:
         """Merge all cards in the View on top of each other"""
-        pass
-
-    def _merge(self, direction: str, padding: int = 2) -> list:
-        """Merge all the card in the View in the desired direction"""
         pass
 
     def __str__(self):
         """Make the view look like a collection of playing cards"""
-        merged_cards = self._merge_right()
+        merge_fx = getattr(self, f"_merge_{self._merge_direction}")
+        # Merge playing cards in desired direction
+        merged_cards = merge_fx()
         return convert_layers_to_string(merged_cards)
 
     def sort(self, category: str = "value", order: str = "asc") -> None:
