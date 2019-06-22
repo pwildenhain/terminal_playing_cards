3bf44668c423f0061fe7ff2f3ce6d28b7b2da1a9
Paul <wildenhaip@email.chop.edu>
2019-06-22 12:56:08 -0400

    :arrow_up: Implement sort methods

--- TerminalPlayingCards/__main__.py
+++ TerminalPlayingCards/__main__.py
@@ -1,11 +1,20 @@
 """Playground for testing module, this file will eventually be deleted"""
 
-from pprint import pprint
 from TerminalPlayingCards import card, view
 
-MY_CARDS = [card.Card("A", "clubs", 0), card.Card("Q", "hearts", 0)]
+MY_CARDS = [
+    card.Card("2", "clubs", value=2),
+    card.Card("Q", "hearts", value=12),
+    card.Card("A", "clubs", value=1),
+    card.Card("K", "spades", value=13),
+    card.Card("2", "diamonds", value=2),
+    card.Card("J", "spades", value=11),
+]
+
 MY_HAND = view.View(MY_CARDS)
 
 if __name__ == "__main__":
-    pprint(str(MY_HAND))
+    print(MY_HAND)
+    # Implement tests for sort method(s)
+    MY_HAND.sort()
     print(MY_HAND)
--- TerminalPlayingCards/view.py
+++ TerminalPlayingCards/view.py
@@ -80,6 +80,46 @@ class View:
         merged_cards = merge_fx()
         return convert_layers_to_string(merged_cards)
 
-    def sort(self, category: str = "value", order: str = "asc") -> None:
-        """Sort the cards in the View according value, suit, or both"""
-        pass
+    @staticmethod
+    def _sort_by_value(cards: list, order: str) -> list:
+        """Sort cards according to value. Use selection sort"""
+        unsorted_cards = cards.copy()
+        sorted_cards = []
+        sort_fx = min if order == "asc" else max
+        for _ in cards:
+            smallest_card = sort_fx(unsorted_cards)
+            unsorted_cards.remove(smallest_card)
+            sorted_cards.append(smallest_card)
+        return sorted_cards
+
+    @staticmethod
+    def _sort_by_suit(cards: list, order: list) -> list:
+        """Sort cards according to suit"""
+        unsorted_cards = cards.copy()
+        sorted_cards = []
+        for suit in order:
+            cards_by_suit = [card for card in unsorted_cards if card.suit == suit]
+            # Skip empty lists for suits not in cards
+            if cards_by_suit == []:
+                continue
+            for card in cards_by_suit:
+                sorted_cards.append(card)
+
+        return sorted_cards
+
+    def sort(
+        self, sort_order: list = None, value_order: str = None, suit_order: list = None
+    ) -> None:
+        """Sort the cards in the View"""
+        # Set default arguments
+        sort_order = ["suit", "value"] if not sort_order else sort_order
+        value_order = "asc" if not value_order else value_order
+        suit_order = (
+            ["clubs", "diamonds", "spades", "hearts", "none"]
+            if not suit_order
+            else suit_order
+        )
+        for sort_option in sort_order:
+            sort_fx = getattr(self, f"_sort_by_{sort_option}")
+            order_params = locals().get(f"{sort_option}_order")
+            self.cards = sort_fx(self.cards, order_params)
