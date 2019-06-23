29676f5a35060f94862d4e9e53425e61bd4b2dae
Paul <wildenhaip@email.chop.edu>
2019-06-22 23:29:45 -0400

    :white_check_mark: Add tests for View.sort()

--- tests/test_view.py
+++ tests/test_view.py
@@ -13,6 +13,36 @@ def two_card_view():
     return View(cards)
 
 
+@pytest.fixture
+def five_card_view():
+    from TerminalPlayingCards.card import Card
+
+    cards = [
+        Card("2", "clubs", value=2),
+        Card("Q", "hearts", value=12),
+        Card("A", "clubs", value=1),
+        Card("K", "spades", value=13),
+        Card("2", "diamonds", value=2),
+        Card("J", "spades", value=11),
+    ]
+    return View(cards)
+
+
+@pytest.fixture
+def five_card_view_sorted():
+    from TerminalPlayingCards.card import Card
+
+    cards = [
+        Card("A", "clubs", value=1),
+        Card("2", "clubs", value=2),
+        Card("2", "diamonds", value=2),
+        Card("J", "spades", value=11),
+        Card("K", "spades", value=13),
+        Card("Q", "hearts", value=12),
+    ]
+    return View(cards)
+
+
 def test_view_str_value(two_card_view):
     """Ensure the proper string value of the given View"""
     two_card_view_string = (
@@ -39,3 +69,36 @@ def test_view_property_getters(two_card_view):
     two_card_view.merge_padding = 3
     assert two_card_view.merge_direction == "left"
     assert two_card_view.merge_padding == 3
+
+
+def test_view_default_sort(five_card_view, five_card_view_sorted):
+    five_card_view.sort()
+    for card in range(5):
+        assert five_card_view[card] == five_card_view_sorted[card]
+        assert five_card_view[card].suit == five_card_view_sorted[card].suit
+
+
+def test_view_sort_by_value(five_card_view, five_card_view_sorted):
+    five_card_view.sort(sort_order=["value"], value_order="desc")
+    five_card_view_sorted.sort(sort_order=["value"], value_order="desc")
+    for card in range(5):
+        assert five_card_view[card] == five_card_view_sorted[card]
+
+
+def test_view_sort_by_suit(five_card_view, five_card_view_sorted):
+    suit_order = ["hearts", "spades", "diamonds", "clubs"]
+    five_card_view.sort(sort_order=["suit"], suit_order=suit_order)
+    five_card_view_sorted.sort(sort_order=["suit"], suit_order=suit_order)
+    for card in range(5):
+        assert five_card_view[card].suit == five_card_view_sorted[card].suit
+
+
+def test_view_sort_by_only_two_suit(five_card_view, five_card_view_sorted):
+    suit_order = ["diamonds", "spades"]
+    five_card_view.sort(sort_order=["suit"], suit_order=suit_order)
+    five_card_view_sorted.sort(sort_order=["suit"], suit_order=suit_order)
+    for card in range(5):
+        if five_card_view[card].suit in ["diamonds", "spades"]:
+            assert five_card_view[card].suit == five_card_view_sorted[card].suit
+        else:
+            continue
