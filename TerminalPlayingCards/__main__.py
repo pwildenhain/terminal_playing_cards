9065acc97b3002501bd93287ba1a7dbc40e7daa1
pwildenhain <pwildenhain@gmail.com>
2019-08-03 14:49:38 -0400

    :heart: Print all suits when run as module

--- TerminalPlayingCards/__main__.py
+++ TerminalPlayingCards/__main__.py
@@ -4,7 +4,12 @@ from TerminalPlayingCards import deck, view
 
 MY_DECK = deck.Deck()
 
-MY_HAND = view.View([MY_DECK.pop() for _ in range(5)])
+MY_DECK.sort(sort_order=["suit"])
+
+SUITED_VIEWS = [
+    view.View([MY_DECK.pop() for _ in range(13)], spacing=-5) for _ in range(4)
+]
 
 if __name__ == "__main__":
-    print(MY_HAND)
+    for suit_view in SUITED_VIEWS:
+        print(suit_view)
